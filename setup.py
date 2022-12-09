5e110646a671d5af2a620b9d79ade40a80d123a5
ejgardner_adrestia <ejgardner@adrestia.com>
2022-12-09 15:23:35 +0000

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='general_utilities',
     version='',
-    packages=['utilities'],
+    packages=['general_utilities', 'general_utilities.linear_model', 'general_utilities.thread_utility'],
     url='',
     license='',
     author='ejgardner',
