66955c5063b3817909dc3b5da34b19b2a50542bd
ejgardner_adrestia <ejgardner@adrestia.com>
2022-12-09 15:19:30 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='general_utilities',
+    version='',
+    packages=['utilities'],
+    url='',
+    license='',
+    author='ejgardner',
+    author_email='',
+    description=''
+)
