fe04d1d7a2e1b5be7a0648035567b55dc1604745
ejgardner_adrestia <ejgardner@adrestia.com>
2023-01-11 16:13:48 +0000

    Added the ability to print to stderr for status messages to thread_utility.py

--- general_utilities/thread_utility/thread_utility.py
+++ general_utilities/thread_utility/thread_utility.py
@@ -1,4 +1,6 @@
 import math
+import sys
+
 import dxpy
 
 from concurrent import futures
@@ -7,10 +9,11 @@ from concurrent.futures import ThreadPoolExecutor
 
 class ThreadUtility:
 
-    def __init__(self, threads: int, error_message, incrementor=500, thread_factor=4):
+    def __init__(self, threads: int, error_message, incrementor=500, thread_factor=4, output_writer=sys.stdout):
 
         self._error_message = error_message
         self._incrementor = incrementor
+        self._output_writer = output_writer
         self._already_collected = False  # A flag to make sure we don't submit jobs to a closed executor
         self._num_jobs = 0
         available_workers = math.floor((threads - 1) / thread_factor)
@@ -27,7 +30,8 @@ class ThreadUtility:
 
     def collect_futures(self) -> list:
         self._already_collected = True
-        print("{0:65}: {val}".format("Total number of threads to iterate through", val = self._num_jobs))
+        print("{0:65}: {val}".format("Total number of threads to iterate through", val=self._num_jobs),
+              file=self._output_writer)
         total_finished_models = 0
         future_results = []
         for future in futures.as_completed(self._future_pool):
@@ -36,7 +40,8 @@ class ThreadUtility:
                 if math.remainder(total_finished_models, self._incrementor) == 0 \
                         or total_finished_models == self._num_jobs:
                     print(f'{"Total number of threads finished":{65}}: {total_finished_models} / {self._num_jobs} '
-                          f'({((total_finished_models / self._num_jobs) * 100):0.2f}%)')
+                          f'({((total_finished_models / self._num_jobs) * 100):0.2f}%)',
+                          file=self._output_writer)
                 future_results.append(future.result())
             except Exception as err:
                 print(self._error_message)
