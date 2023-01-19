f86bba0a344d38a8f4763064b58f218db60c6c9b
ejgardner_adrestia <ejgardner@adrestia.com>
2023-01-19 10:52:41 +0000

    Attempting to fix livestream of subprocess in run_cmd()

--- general_utilities/association_resources.py
+++ general_utilities/association_resources.py
@@ -37,7 +37,7 @@ def run_cmd(cmd: str, is_docker: bool = False, data_dir: str = '/home/dnanexus/'
         proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if livestream_out:
             for line in iter(proc.stdout.readline, b""):
-                sys.stdout.buffer.write(line)
+                print(f'SUBPROCESS STDOUT: {line}\n')
 
             if proc.returncode != 0:
                 print("The following cmd failed:")
