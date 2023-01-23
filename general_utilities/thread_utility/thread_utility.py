36570fe59459b39a14d6cc2bf84325bf9dd53ef4
ejgardner_adrestia <ejgardner@adrestia.com>
2023-01-23 14:33:12 +0000

    Trying to remove memory leaks from ThreadUtility

--- general_utilities/thread_utility/thread_utility.py
+++ general_utilities/thread_utility/thread_utility.py
@@ -1,8 +1,10 @@
 import sys
 import math
+from time import sleep
+from typing import Any, Iterator
+
 import dxpy
 
-from typing import List, Any
 from typing.io import TextIO
 from concurrent import futures
 from concurrent.futures import ThreadPoolExecutor
@@ -18,6 +20,7 @@ class ThreadUtility:
         self._output_writer = output_writer
         self._already_collected = False  # A flag to make sure we don't submit jobs to a closed executor
         self._num_jobs = 0
+        self._total_finished_models = 0
         available_workers = math.floor((threads - 1) / thread_factor)
         self._executor = ThreadPoolExecutor(max_workers=available_workers)
         self._future_pool = []
@@ -58,26 +61,17 @@ class ThreadUtility:
             self._future_pool.append(self._executor.submit(class_type,
                                                            **kwargs))
 
-    def collect_futures(self) -> List[Any]:
-        self._already_collected = True
-        print("{0:65}: {val}".format("Total number of threads to iterate through", val=self._num_jobs),
-              file=self._output_writer)
-        total_finished_models = 0
-        future_results = []
-        for future in futures.as_completed(self._future_pool):
-            try:
-                total_finished_models += 1
-                if math.remainder(total_finished_models, self._incrementor) == 0 \
-                        or total_finished_models == self._num_jobs:
-                    print(f'{"Total number of threads finished":{65}}: {total_finished_models} / {self._num_jobs} '
-                          f'({((total_finished_models / self._num_jobs) * 100):0.2f}%)',
-                          file=self._output_writer)
-                future_results.append(future.result())
-            except Exception as err:
-                print(self._error_message)
-                print(Exception, err)
-                raise dxpy.AppError(self._error_message)
+    # This is a utility method that will make essentially 'hold' until all threads added to this class are completed.
+    # It just makes it so if one does not need to access the futures, there is no need to implement an empty for loop
+    # in your code.
+    def collect_futures(self) -> None:
+        for _ in self:
+            pass
 
-        return future_results
-
-
+    def _check_and_format_progress_message(self):
+        self._total_finished_models += 1
+        if math.remainder(self._total_finished_models, self._incrementor) == 0 \
+                or self._total_finished_models == self._num_jobs:
+            print(f'{"Total number of threads finished":{65}}: {self._total_finished_models} / {self._num_jobs} '
+                  f'({((self._total_finished_models / self._num_jobs) * 100):0.2f}%)',
+                  file=self._output_writer)
