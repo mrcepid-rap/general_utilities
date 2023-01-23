1cc2595bb3c47567164f52130f890cd085fa5815
ejgardner_adrestia <ejgardner@adrestia.com>
2023-01-23 17:11:30 +0000

    Implementing logging in threadutility

--- general_utilities/thread_utility/thread_utility.py
+++ general_utilities/thread_utility/thread_utility.py
@@ -1,11 +1,9 @@
-import sys
 import math
-from time import sleep
-from typing import Any, Iterator
-
 import dxpy
+import logging
 
-from typing.io import TextIO
+from typing import Any, Iterator
+from time import sleep
 from concurrent import futures
 from concurrent.futures import ThreadPoolExecutor
 
@@ -13,17 +11,17 @@ from concurrent.futures import ThreadPoolExecutor
 class ThreadUtility:
 
     def __init__(self, threads: int, error_message: str = "A ThreadUtility thread failed.", incrementor: int = 500,
-                 thread_factor: int = 4, output_writer: TextIO = sys.stdout):
+                 thread_factor: int = 4):
 
         self._error_message = error_message
         self._incrementor = incrementor
-        self._output_writer = output_writer
         self._already_collected = False  # A flag to make sure we don't submit jobs to a closed executor
         self._num_jobs = 0
         self._total_finished_models = 0
         available_workers = math.floor((threads - 1) / thread_factor)
         self._executor = ThreadPoolExecutor(max_workers=available_workers)
         self._future_pool = []
+        logging.basicConfig(level=logging.INFO)
 
     # I have a feeling the sleep() part is VERY inefficient but I am unsure of how to fix at the moment...
     # Due to how futures work, most of the time a next() call will receive a 'None' return. We need to create a
@@ -37,7 +35,6 @@ class ThreadUtility:
 
         self._check_and_format_progress_message()
         result = curr_future.result()
-        del curr_future
         return result
 
     def __iter__(self) -> Iterator:
@@ -47,8 +44,7 @@ class ThreadUtility:
         if len(self._future_pool) == 0:
             raise dxpy.AppError('No jobs submitted to future pool!')
 
-        print("{0:65}: {val}".format("Total number of threads to iterate through", val=self._num_jobs),
-              file=self._output_writer)
+        logging.info("{0:65}: {val}".format("Total number of threads to iterate through", val=self._num_jobs))
 
         self._future_iterator = futures.as_completed(self._future_pool)
         return self
@@ -72,6 +68,6 @@ class ThreadUtility:
         self._total_finished_models += 1
         if math.remainder(self._total_finished_models, self._incrementor) == 0 \
                 or self._total_finished_models == self._num_jobs:
-            print(f'{"Total number of threads finished":{65}}: {self._total_finished_models} / {self._num_jobs} '
-                  f'({((self._total_finished_models / self._num_jobs) * 100):0.2f}%)',
-                  file=self._output_writer)
+            logging.info(f'{"Total number of threads finished":{65}}: {self._total_finished_models} / {self._num_jobs} '
+                         f'({((self._total_finished_models / self._num_jobs) * 100):0.2f}%)',
+                         file=self._output_writer)
