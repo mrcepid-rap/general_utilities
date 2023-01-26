ee017ee10a7edc2ae3a4b7b49ba6f06a2d45ce8b
ejgardner_adrestia <ejgardner@adrestia.com>
2023-01-26 11:56:25 +0000

    Added requirements.txt and modified setup.py accordingly

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+def load_requirements(fname: Path):
+    reqs = []
+    with fname.open('r') as reqs_file:
+        for line in reqs_file:
+            reqs.append(line.rstrip())
+    return reqs
+
 
 setup(
     name='general_utilities',
@@ -8,5 +18,6 @@ setup(
     license='',
     author='ejgardner',
     author_email='',
-    description=''
+    description='',
+    install_requires=load_requirements(Path("requirements.txt"))
 )
