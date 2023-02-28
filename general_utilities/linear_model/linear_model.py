dad9019e7f48140c19f8ff1ab27a1ed06ff6a793
ejgardner_adrestia <ejgardner@adrestia.com>
2023-02-28 14:31:30 +0000

    Fixed import statement for files

--- general_utilities/linear_model/linear_model.py
+++ general_utilities/linear_model/linear_model.py
@@ -6,6 +6,7 @@ import statsmodels.api as sm
 from pathlib import Path
 from typing import Tuple, List
 
+from importlib_resources import files
 from general_utilities.association_resources import get_chromosomes, run_cmd
 
 
