bb8a168c1ee15cafedd78558951829ac4c70384e
ejgardner_adrestia <ejgardner@adrestia.com>
2023-08-14 17:29:11 +0100

    Massive refactor of plot_lib:
    * Plotter interface created to create common set of plotting tools
    * How plots are created depending on plot type has changed

--- general_utilities/plot_lib/manhattan_plotter.py
+++ general_utilities/plot_lib/manhattan_plotter.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
+from typing import Dict, Tuple, List
+from pathlib import Path
 
-class ManhattanPlotter:
+from adrestia_utilities.import_utils.imputation_loader import GeneticData
+from general_utilities.job_management.command_executor import CommandExecutor
+from general_utilities.plot_lib.cluster_plotter import ClusterPlotter
 
-    def __init__(self, results_table: pd.DataFrame, labels: pd.DataFrame):
+
+class ManhattanPlotter(ClusterPlotter):
+
+    def __init__(self, cmd_executor: CommandExecutor, results_table: pd.DataFrame, genetic_data: Dict[str, GeneticData],
+                 chrom_column: str, pos_column: str, alt_column: str, id_column: str, p_column: str, csq_column: str,
+                 maf_column: str, gene_symbol_column: str):
+        super().__init__(cmd_executor, results_table, genetic_data, chrom_column, pos_column, alt_column, id_column,
+                         p_column, csq_column, maf_column, gene_symbol_column)
+
+    def plot(self) -> Tuple[List[Path], List[Path]]:
 
         pass
