4098eff8d85d2e2d81a11c2856c6a2bf31fd6a3d
ejgardner_adrestia <ejgardner@adrestia.com>
2023-11-10 12:55:53 +0000

    Trying to fix subjob utility

--- general_utilities/job_management/subjob_test.py
+++ general_utilities/job_management/subjob_test.py
@@ -38,7 +38,7 @@ def test_subjob(tabix_dxfile: dxpy.DXFile):
     LOGGER.info('Attempting to create subjobs...')
     subjob_launcher = SubjobUtility()
     for chr in range(1,23):
-        subjob_launcher.launch_job('general_utilities.subjob_test.tabix_subjob',
+        subjob_launcher.launch_job('tabix_subjob',
                                    inputs={'input_table': {'$dnanexus_link': bgzip_dxlink.get_id()}, 'chromosome': chr},
                                    outputs=['chromosome'])
 
@@ -48,11 +48,3 @@ def test_subjob(tabix_dxfile: dxpy.DXFile):
         LOGGER.info(f'This chromosome worked: {output}')
 
 
-@dxpy.entry_point('tabix_subjob')
-def run_subjob(input_table: dict, chromosome: str):
-
-    download_dxfile_by_name(input_table, print_status=True)
-
-    output = {'chromosome': chromosome}
-
-    return output
