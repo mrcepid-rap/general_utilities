ee63478bdcb3a69d25181558bd40d2df75da8223
eugene.gardner <eugene.gardner@insmed.com>
2025-08-04 13:01:57 +0100

    testing csr_matrix creation

--- general_utilities/bgen_utilities/genotype_matrix.py
+++ general_utilities/bgen_utilities/genotype_matrix.py
@@ -142,6 +142,7 @@ def generate_csr_matrix_from_bgen(bgen_path: Path, sample_path: Path, variant_fi
 
         # convert this to a csr matrix
         print(stacked_variants)
+        print(len(bgen_reader.samples))
         final_genotypes = csr_matrix(stacked_variants, shape=(len(bgen_reader.samples), variant_n))
 
     return final_genotypes, summary_dict
