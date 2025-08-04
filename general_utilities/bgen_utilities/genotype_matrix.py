7afebbe5631473b498b0d88d7193a1685d0d16b9
eugene.gardner <eugene.gardner@insmed.com>
2025-08-04 10:57:02 +0100

    Implemented separate make_variant_list method

--- general_utilities/bgen_utilities/genotype_matrix.py
+++ general_utilities/bgen_utilities/genotype_matrix.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Tuple, Dict, Any
+from typing import Tuple, Dict, Any, Set, TypedDict
 
 import numpy as np
 import pandas as pd
@@ -10,15 +10,57 @@ from scipy.sparse import csr_matrix
 
 LOGGER = MRCLogger(__name__).get_logger()
 
+class GeneInformation(TypedDict):
+    """TypedDict to hold information about a gene and it's variants."""
 
-def generate_csr_matrix_from_bgen(variant_list: pd.DataFrame, bgen_path: Path, sample_path: Path,
+    chrom: str
+    min: int
+    max: int
+    vars: Set[str]
+
+
+def make_variant_list(variant_list: pd.DataFrame) -> Dict[str, GeneInformation]:
+    """Extracts a list of variants for all ENSTs in a pandas DataFrame.
+
+    The provided DataFrame MUST have the columns: 'ENST', 'CHROM', 'POS', and 'varID'. This format is the standard
+    file that is produced by the CollapseVariants tool, and is provided in output tar files as
+    '*.STAAR.variants_table.tsv'.
+
+    :param variant_list: DataFrame containing variants.
+    :return: A pandas DataFrameGroupBy object where each group corresponds to a unique ENST, and each ENST has information
+        about its chromosome, minimum and maximum positions, and a set of variant IDs.
+    """
+
+    # First aggregate across genes to generate a list of genes and their respective variants
+    search_list = variant_list.groupby('ENST').aggregate(
+        CHROM=('CHROM', 'first'),
+        MIN=('POS', 'min'),
+        MAX=('POS', 'max'),
+        VARS=('varID', set)
+    )
+
+    # Format into a dictionary to make the information more accessible
+    gene_dict = {}
+    for current_gene in search_list.itertuples():
+
+        gene_dict[current_gene.Index] = GeneInformation(
+            chrom=current_gene.CHROM,
+            min=current_gene.MIN,
+            max=current_gene.MAX,
+            vars=current_gene.VARS
+        )
+
+    return gene_dict
+
+
+def generate_csr_matrix_from_bgen(bgen_path: Path, sample_path: Path, variant_filter_list: Set[str],
+                                  chromosome: str = None, start: int = None, end: int = None,
                                   should_collapse_matrix: bool = True) -> Tuple[csr_matrix, Dict[str, Any]]:
     """
     Convert BGEN genotypes into a sparse matrix format.
 
     Creates a CSR matrix from BGEN file genotypes for use in STAAR/GLM association testing.
     The matrix represents samples as rows and either genes or variants as columns.
-    Non-alternate genotypes are filtered out during conversion.
 
     Note that in order to optimise this process we are collapsing the variants here (stored in the CSR matrix part
     of our output tuple). We are also saving gene and sample level variant information in a dictionary,
@@ -27,9 +69,12 @@ def generate_csr_matrix_from_bgen(variant_list: pd.DataFrame, bgen_path: Path, s
     matrix in a CSR format. This is used in the associationtesting modules downstream. The rationale for doing it this
     way is that we can save memory when collapsing, but still be able to call the un-collapsed matrices when needed.
 
-    :param variant_list: DataFrame containing variants to extract.
     :param bgen_path: Path to the BGEN file.
     :param sample_path: Path to the sample file.
+    :param variant_filter_list: A set of variant IDs to extract from the BGEN file.
+    :param chromosome: Chromosome to filter variants by (optional).
+    :param start: Start position to filter variants by (optional).
+    :param end: End position to filter variants by (optional).
     :param should_collapse_matrix: If True (default) collapse variants - sum variants per gene.
         If False, don't sum variants per gene and return the matrix instead.
     :return: A tuple containing:
@@ -37,76 +82,65 @@ def generate_csr_matrix_from_bgen(variant_list: pd.DataFrame, bgen_path: Path, s
         - summary_dict: A dictionary with summary information for each gene.
     """
 
-    # First aggregate across genes to generate a list of genes and their respective variants
-    search_list = variant_list.groupby('ENST').aggregate(
-        CHROM=('CHROM', 'first'),
-        MIN=('POS', 'min'),
-        MAX=('POS', 'max'),
-        VARS=('varID', list)
-    )
-
     with BgenReader(bgen_path, sample_path=sample_path, delay_parsing=True) as bgen_reader:
-        current_samples = np.array(bgen_reader.samples)
 
-        # create a list to store genotype arrays
-        genotype_arrays = []
-        summary_dict = {}
+        # Fetch actual data from the BGEN file
+        # Handle chromosome, start, and end filtering
+        if chromosome is not None and start is None and end is None:
+            # chromosome is provided, but not start or end
+            variants = bgen_reader.fetch(chromosome)  # Fetch all variants on the specified chromosome
+
+        elif [chromosome, start, end].count(None) == 2:
+            # Unclear what the user wants if they have provided two of the three
+            raise ValueError("If start or end is provided, chromosome must also be provided.")
+
+        else:
+            variants = bgen_reader.fetch(chromosome, start, end)
+
+        # create a store for the variant level information
+        variant_arrays = []
         variant_n = 0
 
-        # iterate through each gene in our search list
-        for gene_n, current_gene in enumerate(search_list.itertuples()):
+        # collect genotype arrays for each variant
+        for current_variant in variants:
 
-            # Fetch actual data from the BGEN file
-            variants = bgen_reader.fetch(current_gene.CHROM, current_gene.MIN, current_gene.MAX)
+            if variant_filter_list is not None and current_variant.rsid not in variant_filter_list:
+                # if we have a variant filter list, skip variants that are not in the filter list Don't ask me why
+                # the not / not logic works here, but it does so don't change it I think it's because python parses
+                # the variant_filter_list 'not' first and if it fails it doesn't continue to the second 'not'
+                # statement, which would otherwise raise an error when using 'in' for NoneType.
+                continue
 
-            # create a store for the variant level information
-            variant_arrays = []
+            variant_n += 1 # Have to increment variant_n here, as enumerate would capture skipped variants
 
-            gene_variant_start = variant_n
-            # collect genotype arrays for each variant
-            for current_variant in variants:
+            # pull out the actual genotypes
+            current_probabilities = current_variant.probabilities
 
-                if current_variant.rsid in current_gene.VARS:
-                    variant_n += 1
-                    # pull out the actual genotypes
-                    current_probabilities = current_variant.probabilities
+            # store variant codings
+            variant_array = np.where(current_probabilities[:, 1] == 1, 1,
+                                     np.where(current_probabilities[:, 2] == 1, 2, 0))
 
-                    # store variant codings
-                    variant_array = np.where(current_probabilities[:, 1] == 1, 1,
-                                             np.where(current_probabilities[:, 2] == 1, 2, 0))
+            # store variant level information in the array we created
+            variant_arrays.append(variant_array)
 
-                    # store variant level information in the array we created
-                    variant_arrays.append(variant_array)
+        # stack the variant information for all variants in the gene
+        stacked_variants = np.column_stack(variant_arrays)
 
-            # stack the variant information for all variants in the gene
-            stacked_variants = np.column_stack(variant_arrays)
+        # if we are collapsing here (to save on memory), leave as False. If set to True, we won't collapse
+        # and instead the uncollapsed stacked variants will be appended
+        # note the vector naming convention in this small section is a bit hacky, but we want the vectors naming
+        # to be consistent so it works for the rest of the function
+        if should_collapse_matrix is True:
+            stacked_variants = stacked_variants.sum(axis=1)
+            variant_n = 1
 
-            # if we are collapsing here (to save on memory), leave as False. If set to True, we won't collapse
-            # and instead the uncollapsed stacked variants will be appended
-            # note the vector naming convention in this small section is a bit hacky but we want the vectors naming
-            # to be consistent so it works for the rest of the function
-            if should_collapse_matrix is True:
-                stacked_variants = stacked_variants.sum(axis=1)
-                search_list = gene_n + 1
-                gene_n = [gene_n]
-            else:
-                gene_n = [var for var in range (gene_variant_start, variant_n)]
-                search_list = variant_n
-
-            # append the variant arrays to the genotype array
-            genotype_arrays.append(stacked_variants)
-
-            # record the per-gene stats in a dict
-            summary_dict[current_gene.Index] = {
-                'sum': np.sum(stacked_variants),  # use np.sum to get total of all values
-                'variants_per_gene': len(variant_arrays),  # get number of variants
-                'gene_index': gene_n,
-            }
-
-        # stack all genotype arrays into a matrix (samples × variants)
-        final_genotypes = np.column_stack(genotype_arrays)
+        # record the collapsing stats in a dict
+        summary_dict = {
+            'sum': np.sum(stacked_variants),  # use np.sum to get total of all values
+            'variants_in_matrix': len(variant_arrays),  # get number of variants, based on pre-collapse number of variants!
+        }
 
         # convert this to a csr matrix
-        final_genotypes = csr_matrix(final_genotypes, shape=(len(current_samples), search_list))
+        final_genotypes = csr_matrix(stacked_variants, shape=(len(bgen_reader.samples), variant_n))
 
     return final_genotypes, summary_dict
